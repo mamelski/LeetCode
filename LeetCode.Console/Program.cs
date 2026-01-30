767e19b9b3b0cc26cd5d5952b33a35929532dbdc
Jakub Mamelski <jakub.mamelski@outlook.com>
2026-01-30 19:27:20 +0100

    88. Merge Sorted Array

--- LeetCode.Console/Program.cs
+++ LeetCode.Console/Program.cs
@@ -1,7 +1,13 @@
-﻿using LeetCode.Console.Problems._109._Convert_Sorted_List_to_Binary_Search_Tree;
+﻿using LeetCode.Console.Problems._88._Merge_Sorted_Array;
 
 var solution = new Solution();
 
-var x = solution.SortedListToBST(new ListNode(-10, new ListNode(-3, new ListNode(0, new ListNode(5, new ListNode(9))))));
 
-Console.WriteLine(x);
+int[] num1 = [0];
+int[] num2 = [1];
+Solution.Merge(num1, 0, num2, 1);
+
+foreach (var item in num1)
+{
+    Console.WriteLine(item);
+}
